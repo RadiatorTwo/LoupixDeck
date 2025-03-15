a114282c627e8cdcb074c88ef2ff118249707879
RadiatorTwo <radiatortwo@gmail.com>
2025-03-15 21:24:33 +0100

    Better Styling for the Buttons

--- Models/TouchButtonPage.cs
+++ Models/TouchButtonPage.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Styling;
 
 namespace LoupixDeck.Models
 {
-    public class TouchButtonPage
+    public class TouchButtonPage : AvaloniaObject
     {
         public TouchButtonPage()
         {
@@ -18,6 +22,15 @@ namespace LoupixDeck.Models
             TouchButtons = new TouchButton[pageSize];
         }
 
+        public static readonly StyledProperty<bool> IsSelectedProperty =
+            AvaloniaProperty.Register<TouchButtonPage, bool>(nameof(IsSelected));
+        
+        public bool IsSelected
+        {
+            get => GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+        
         public int Page { get; set; }
         public TouchButton[] TouchButtons { get; set; }
     }
