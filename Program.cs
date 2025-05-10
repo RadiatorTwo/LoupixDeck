e52995ec1ac2581849321310ed67bfc77e60f61b
RadiatorTwo <radiatortwo@gmail.com>
2025-05-10 14:24:01 +0200

    Check for already Running Instance at start (#82)

--- Program.cs
+++ Program.cs
@@ -1,15 +1,66 @@
-﻿using Avalonia;
+﻿using System.Net;
+using System.Net.Sockets;
+using Avalonia;
 
 namespace LoupixDeck;
 
 sealed class Program
 {
+#if !WINDOWS
+    private const string SocketPath = "/tmp/loupixdeck_app.sock";
+    private static Socket _listenerSocket;
+#else
+    private const string MutexName = "LoupixDeck_Mutex";
+    private static Mutex _instanceMutex;
+#endif
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+#if !WINDOWS
+        {
+            if (File.Exists(SocketPath))
+            {
+                try
+                {
+                    using var client = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+                    client.Connect(new UnixDomainSocketEndPoint(SocketPath));
+                    Console.WriteLine("Already running.");
+                    return;
+                }
+                catch (SocketException)
+                {
+                    File.Delete(SocketPath);
+                }
+            }
+
+            _listenerSocket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+            _listenerSocket.Bind(new UnixDomainSocketEndPoint(SocketPath));
+            _listenerSocket.Listen(1);
+
+            AppDomain.CurrentDomain.ProcessExit += (_, _) =>
+            {
+                _listenerSocket.Close();
+                File.Delete(SocketPath);
+            };
+        }
+#else
+        _instanceMutex = new Mutex(false, MutexName);
+        if (!_instanceMutex.WaitOne(0, false))
+        {
+            Console.WriteLine("Already running.");
+            return;
+        }
+
+        AppDomain.CurrentDomain.ProcessExit += (_, _) => _instanceMutex.ReleaseMutex();
+#endif
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
